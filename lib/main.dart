5454c36b42719b48d677f6bd1a5e03f4f666bdf1
anthonykid <dickya630@gmail.com>
2021-06-14 23:11:39 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Rainbow(), // Panggil FirstScreen di sini
+    );
+  }
+}
+
+class Rainbow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.red,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.orange,
+          ),
+        ),
+        Expanded(
+          //flex: 2, this can make the selected colors to have 2x bigger than the other, because default is flex 1
+          child: Container(
+            color: Colors.yellow,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.indigo,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.purple,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// Flutter memiliki widget Expanded yang dapat mengembangkan child dari Row atau Column sesuai dengan ruang yang tersedia. Cara menggunakannya Anda cukup membungkus masing-masing child ke dalam Expanded.
